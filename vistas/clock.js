5ecb7715c1a97d36a017a6e09b2e4c9a0b5a0ec5
Jorge Antony Milian Montalvo <antonymilian19@gmail.com>
2023-05-09 11:49:27 -0500

    09-05-2023

--- vistas/clock.js
+++ vistas/clock.js
@@ -1,20 +1,21 @@
 const time = document.getElementById('time');
 const date = document.getElementById('date');
 
-const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
-];
-const dayshNames = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    const daysNames = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+
 
 const interval = setInterval(() => {
 
     const local = new Date();
     
     let day = local.getDate(),
+        days = local.getDay(),
         month = local.getMonth(),
         year = local.getFullYear();
+       
 
     time.innerHTML = local.toLocaleTimeString();
-    date.innerHTML = `${dayshNames[day]} ${day} de ${monthNames[month]} ${year}`;
+    date.innerHTML = `${daysNames[days]} ${day} de ${monthNames[month]} ${year}`;
 
-}, 1000);
+});
