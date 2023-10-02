cfd61f7e0344942137a2d2668af10d9cfbe3a28d
AntonyMilian190793 <51470801+AntonyMilian190793@users.noreply.github.com>
2023-10-02 07:52:34 -0500

    02-10-2023

--- vistas/footer.php
+++ vistas/footer.php
@@ -25,7 +25,6 @@
     <script src="../public/js/bootbox.min.js"></script> 
     <script src="../public/js/bootstrap-select.min.js"></script>
     <script src="../public/js/filestyle.min.js"> </script>
-    <script src="../public/js/filestyle.min.js"> </script>
 
 </body> 
 </html>
